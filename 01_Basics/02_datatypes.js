7e40ff66f56c2197daaa476128f7f78951833763
Beaston88 <114310698+Beaston88@users.noreply.github.com>
2024-05-16 03:27:46 +0000

    Learn More with Datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) it is allowed in browsers but when we are using directly JS
+
+
+
+console.log(3 
+    +
+    3) // Code readability must be taken care of
+console.log("Ankit")
+
+let name = "Hitesh"
+let age = 18
+let isLoggedIn = false
+
+//use of typeof
+console.log(typeof("Hitesh")); 
+
+// Primitive datatypes:
+// number ==> range : 2^53
+// bigint 
+// string ==> ""
+// boolean ==> true/false
+// null ==> standalone value
+// undefined ==> we have not assigned any value
+// symbol ==> unique
+
+
+//Non-primitive datatypes:
+//object:
+console.log(typeof(null)); //It is a type of object
+console.log(typeof(undefined)); //It is basically undefined 
+
