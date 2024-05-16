192e7c6ecc0903ec13c3bb20fb973e58fca478bd
Beaston88 <114310698+Beaston88@users.noreply.github.com>
2024-05-16 04:20:12 +0000

    Comparisons...

---
+++ 01_Basics/04_comparisons.js
@@ -0,0 +1,13 @@
+// console.log("2">1) // JS automatically converted string to number for comparison
+// console.log("02">1)
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0); // true //always compare between same datatypes
+
+// console.log(undefined>0);
+// console.log(undefined==0);
+// console.log(undefined>=0); //false // always
+
+//strict check - ===
+console.log("2"===2);
