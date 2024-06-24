6b3cd60f8748cdecb50799ac0c9244bf30fcb655
Beaston88 <114310698+Beaston88@users.noreply.github.com>
2024-06-24 02:28:34 +0000

    Digital Clock

---
+++ Projects/Project3/script.js
@@ -0,0 +1,9 @@
+// const clock = document.getElementById('#clock');
+const clock = document.querySelector('#clock')
+
+// console.log(date.toLocaleTimeString());
+// clock.innerHTML = date.toLocaleTimeString();
+setInterval( function() {
+    let date = new Date();
+    clock.innerHTML = date.toLocaleTimeString();
+},1000);
